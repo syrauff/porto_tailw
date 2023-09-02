f348670bd5c8e0dc073d3eec71f2a16e5083b802
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2023-09-02 13:06:14 +0800

    porto2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center: true,
+      padding: '16px'
+    },
     extend: {
       colors: {
         primary: '#337CCF',
       },
+      screens: {
+        '2xl' : '1320px'
+      }
     },
   },
   plugins: [],
