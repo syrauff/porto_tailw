911742177621a11c21b877188121fa2267637b8b
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2023-09-07 22:01:28 +0800

    updt7sep

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,29 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden')
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeInUpElements = document.querySelectorAll(".achiv");
+  
+    function checkFadeInUp() {
+      fadeInUpElements.forEach(function(element) {
+        var elementTop = element.getBoundingClientRect().top;
+        var windowHeight = window.innerHeight;
+  
+        if (elementTop < windowHeight) {
+          element.style.opacity = "1";
+          element.style.transform = "translateY(0)";
+        }
+        else if(elementTop > windowHeight) {
+            element.style.opacity = "0";
+          element.style.transform = "translateY(20px)";
+        }
+      });
+    }
+  
+    // Tambahkan event listener untuk memeriksa saat pengguna menggulir halaman
+    window.addEventListener("scroll", checkFadeInUp);
+  
+    // Panggil fungsi untuk memeriksa elemen yang sudah terlihat saat halaman dimuat
+    checkFadeInUp();
+  });
